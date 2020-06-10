360f197abad71809a7784e0a868fe02fd2a079e1
dwandrew <danwandrew@gmail.com>
2020-06-10 09:45:25 +0100

    Done.

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,13 +1,24 @@
+require_relative 'errors'
+
 class Person
   attr_accessor :partner, :name
+  include CustomErrors
 
   def initialize(name)
     @name = name
   end
 
   def get_married(person)
-    self.partner = person
-    person.partner = self
+    if person.class != Person
+      begin
+        raise PartnerError
+      rescue PartnerError => error
+          puts error.message
+      end
+    else
+      person.partner = self
+    end
+
   end
 
 end
