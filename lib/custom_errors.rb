f61fc7bf409e14ceaf03106e656367611eabbe1b
Maloochow <luyao.mly@gmail.com>
2020-06-12 17:47:00 -0400

    Done.

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,3 +1,6 @@
+# require_relative './custome_error_module.rb'
+
+
 class Person
   attr_accessor :partner, :name
 
@@ -7,7 +10,21 @@ class Person
 
   def get_married(person)
     self.partner = person
+    if person.class != Person
+      begin
+      raise DataTypeError
+      rescue DataTypeError => error
+        puts error.message
+      end
+    else
     person.partner = self
+    end
+  end
+
+  class DataTypeError < StandardError
+    def message
+      "you must give the get_married method an argument of an instance of the person class!"
+    end
   end
 
 end
@@ -15,7 +32,3 @@ end
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
-
-
-
-
